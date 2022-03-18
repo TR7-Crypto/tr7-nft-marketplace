7f9b7414b4cf93f8093cc35acbb6bbcdf3c7c63c
Si Truong <si.truongtan@gmail.com>
2022-03-18 21:25:41 +0700

    initial commit

---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    uint256 public tokenCount;
+
+    constructor() ERC721("TR7 NFT", "TR7") {}
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        return (tokenCount);
+    }
+}
