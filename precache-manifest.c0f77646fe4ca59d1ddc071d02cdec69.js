666e006431ddf3619c2a46526a86e41719cdfe4b
Si Truong <si.truongtan@gmail.com>
2022-04-01 21:02:44 +0700

    Updates

---
+++ precache-manifest.c0f77646fe4ca59d1ddc071d02cdec69.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "5597d69a0ba2ec359127",
+    "url": "/tr7-nft-marketplace/static/css/main.8f4f223c.chunk.css"
+  },
+  {
+    "revision": "5597d69a0ba2ec359127",
+    "url": "/tr7-nft-marketplace/static/js/main.f5cbeabf.chunk.js"
+  },
+  {
+    "revision": "5d25deeb47d12f467c33",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerDailyMotion.1ad4f220.chunk.js"
+  },
+  {
+    "revision": "54c2971ade4c0aaf854b",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerFacebook.ffdc33f7.chunk.js"
+  },
+  {
+    "revision": "274b76ad1527168e7b4c",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerFilePlayer.9bde7447.chunk.js"
+  },
+  {
+    "revision": "7be971ca1912642b66ce",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerKaltura.5e9e51fe.chunk.js"
+  },
+  {
+    "revision": "037af31bb001962d608d",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerMixcloud.f9b331b4.chunk.js"
+  },
+  {
+    "revision": "516833ab81641501ec5f",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerPreview.00cd453d.chunk.js"
+  },
+  {
+    "revision": "43b8489967a5325b9cdf",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerSoundCloud.e09a9838.chunk.js"
+  },
+  {
+    "revision": "df5fc308952fe26f58bb",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerStreamable.c4f4fb83.chunk.js"
+  },
+  {
+    "revision": "4f8c66553a101fec9ade",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerTwitch.8d395d50.chunk.js"
+  },
+  {
+    "revision": "884507a1dd75351b58ff",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerVidyard.1fb19ca5.chunk.js"
+  },
+  {
+    "revision": "fbd1849fb1cf0db5580e",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerVimeo.e2e40ece.chunk.js"
+  },
+  {
+    "revision": "2d026a850a394b211137",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerWistia.0c98b8ef.chunk.js"
+  },
+  {
+    "revision": "79943dc0ffcb86392db5",
+    "url": "/tr7-nft-marketplace/static/js/reactPlayerYouTube.a64da35c.chunk.js"
+  },
+  {
+    "revision": "899ed190942b7c9750cc",
+    "url": "/tr7-nft-marketplace/static/js/runtime~main.447eaa1d.js"
+  },
+  {
+    "revision": "62614eaf388c6d29a3ec",
+    "url": "/tr7-nft-marketplace/static/css/15.a20adbde.chunk.css"
+  },
+  {
+    "revision": "62614eaf388c6d29a3ec",
+    "url": "/tr7-nft-marketplace/static/js/15.1566e3b9.chunk.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/tr7-nft-marketplace/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "dd5b9b6f700df9529fc27a2fb7699d74",
+    "url": "/tr7-nft-marketplace/index.html"
+  }
+];
